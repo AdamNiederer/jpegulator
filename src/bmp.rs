2a2b14c71668d18b09b047f114b782290da1dfcc
Adam Niederer <adam.niederer@gmail.com>
2024-03-10 00:39:04 -0500

    Prepare for encoder

--- src/bmp.rs
+++ src/bmp.rs
@@ -1,3 +1,14 @@
+fn swap_bgr_rgb(bgr_rgb: &[u8]) -> Vec<u8> {
+    assert!(bgr_rgb.len() % 3 == 0);
+    let mut rgb_bgr = vec!(0; bgr_rgb.len());
+    for i in 0..bgr_rgb.len() / 3 {
+        rgb_bgr[i * 3] = bgr_rgb[i * 3 + 2];
+        rgb_bgr[i * 3 + 1] = bgr_rgb[i * 3 + 1];
+        rgb_bgr[i * 3 + 2] = bgr_rgb[i * 3];
+    }
+    return rgb_bgr;
+}
+
 pub fn write_bmp(bgr: &[u8], width: u32, height: u32) -> Vec<u8> {
     assert!(bgr.len() % 3 == 0);
 
@@ -22,12 +33,7 @@ pub fn write_bmp(bgr: &[u8], width: u32, height: u32) -> Vec<u8> {
     ret.extend_from_slice(&0u32.to_le_bytes());
     ret.extend_from_slice(&0u32.to_le_bytes());
 
-    let mut rgb = bgr.to_owned();
-    for i in 0..bgr.len() / 3 {
-        let tmp = bgr[i * 3];
-        rgb[i * 3] = bgr[i * 3 + 2];
-        rgb[i * 3 + 2] = tmp;
-    }
+    let rgb = swap_bgr_rgb(bgr);
 
     for i in (0..height).rev() {
         let start = (i * width * 3) as usize;
@@ -39,4 +45,25 @@ pub fn write_bmp(bgr: &[u8], width: u32, height: u32) -> Vec<u8> {
     }
 
     return ret;
+}
+
+pub fn read_bmp(bmp: &[u8]) -> Vec<u8> {
+    assert!(&bmp[0..1] == "BM".as_bytes());
+
+    let width = u16::from_le_bytes((bmp[16], bmp[17]).into()) as usize;
+    let height = u16::from_le_bytes((bmp[18], bmp[19]).into()) as usize;
+    let mut bgr = Vec::with_capacity(width * height);
+
+    // for i in (0..height).rev() {
+    //     let start = (i * width * 3) as usize;
+    //     let end = start + (width * 3) as usize;
+    //     ret.extend_from_slice(&rgb[start..end]);
+    //     for _ in 0..(width % 4) {
+    //         ret.push(0x00);
+    //     }
+    // }
+
+    let rgb = swap_bgr_rgb(&bgr);
+
+    return rgb;
 }
---
+++ src/encoder.rs
@@ -0,0 +1,3 @@
+pub fn encode(input: Vec<u8>) -> Result<Vec<u8>, anyhow::Error> {
+    unimplemented!();
+}
